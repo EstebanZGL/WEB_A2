8468fbbf0599095220d2123336153566008b6616
EstebanZGL <esteban.ziegler@viacesi.fr>
2025-03-26 00:53:01 +0100

    f

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+// Pour le débogage - décommenter pour voir les détails de l'URI
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// echo "URI originale: " . $_SERVER['REQUEST_URI'] . "<br>";
 
 // Charger les fichiers nécessaires
 require_once 'config/database.php';
@@ -7,17 +11,13 @@ require_once 'routes/web.php';
 // Démarrage de l'application
 $uri = trim($_SERVER['REQUEST_URI'], '/');
 
-// Supprimer le préfixe "cesi-lebonplan" de l'URI
-$basePath = 'cesi-lebonplan';
-if (strpos($uri, $basePath) === 0) {
-    $uri = substr($uri, strlen($basePath));
-    $uri = trim($uri, '/');
-}
-
 // Traitement de l'URI pour gérer les paramètres GET
 if (strpos($uri, '?') !== false) {
     $uri = substr($uri, 0, strpos($uri, '?'));
 }
+
+// Pour le débogage - décommenter pour voir l'URI traitée
+// echo "URI traitée: '" . $uri . "'<br>";
 
 // Appel du routeur avec l'URI traitée
 route($uri);
