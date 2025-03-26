d28e46f253dfdbad656e704fb01d8ae0dfb74ff9
EstebanZGL <esteban.ziegler@viacesi.fr>
2025-03-26 01:45:15 +0100

    fse

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// Activer l'affichage des erreurs
+// Activer l'affichage des erreurs pour le débogage
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -8,17 +8,18 @@ require_once 'config/database.php';
 require_once 'routes/web.php';
 
 // Démarrage de l'application
-$uri = trim($_SERVER['REQUEST_URI'], '/');
+$uri = $_SERVER['REQUEST_URI'];
+echo "URI brute: " . $uri . "<br>";
 
-// Afficher l'URI pour le débogage
-echo "URI originale: " . $uri . "<br>";
+// Supprimer le slash initial
+$uri = ltrim($uri, '/');
+echo "URI après ltrim: " . $uri . "<br>";
 
 // Traitement de l'URI pour gérer les paramètres GET
 if (strpos($uri, '?') !== false) {
     $uri = substr($uri, 0, strpos($uri, '?'));
 }
-
-echo "URI traitée: " . $uri . "<br>";
+echo "URI finale: " . $uri . "<br>";
 
 // Appel du routeur avec l'URI traitée
 route($uri);
