46aa911195e24e5c632b60857fa85ee2b0ef8500
EstebanZGL <esteban.ziegler@viacesi.fr>
2025-03-26 01:03:49 +0100

    fesfesfesqdsd

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ function route($uri) {
     $logoutController = new LogoutController();
 
     // Pour le débogage - décommenter si nécessaire
-    // echo "URI reçue par le routeur: '" . $uri . "'<br>";
+    echo "URI reçue par le routeur: '" . $uri . "'<br>";
 
     // Gestion des différentes routes
     switch ($uri) {
