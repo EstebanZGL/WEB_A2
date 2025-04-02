e9da2ae7bccb71ee7700e225f9864dd030a1a0d6
Esteban ZIEGLER <esteban.ziegler@viacesi.fr>
2025-04-03 01:12:47 +0200

    Welcome Message

--- app/views/login/session.php
+++ app/views/login/session.php
@@ -10,10 +10,10 @@ $response = [
     'utilisateur' => isset($_SESSION['utilisateur']) ? $_SESSION['utilisateur'] : null,
     'user_id' => isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null,
     'email' => isset($_SESSION['email']) ? $_SESSION['email'] : null,
-    'prenom' => isset($_SESSION['prenom']) ? $_SESSION['prenom'] : null
+    'prenom' => isset($_SESSION['prenom']) ? htmlspecialchars_decode($_SESSION['prenom'], ENT_QUOTES) : null
 ];
 
-// Envoyer la réponse au format JSON
-header('Content-Type: application/json');
-echo json_encode($response);
+// Définir l'encodage UTF-8 pour la réponse
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($response, JSON_UNESCAPED_UNICODE);
 ?>
