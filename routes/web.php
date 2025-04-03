5fc41044d6ae7a716c28872773d41f806e12f976
Esteban ZIEGLER <esteban.ziegler@viacesi.fr>
2025-04-03 02:22:18 +0200

    test2

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,9 @@ require_once 'app/controllers/GestionController.php';
 require_once 'app/controllers/LogoutController.php';
 require_once 'app/controllers/WishlistController.php';
 require_once 'app/controllers/CandidatureController.php';
-require_once 'app/controllers/DashboardController.php'; // Ajout du contrôleur Dashboard
+require_once 'app/controllers/DashboardController.php'; 
 
 function route($uri) {
-    // Initialisation des contrôleurs
     $homeController = new HomeController();
     $loginController = new LoginController();
     $offresController = new OffresController();
@@ -21,7 +20,7 @@ function route($uri) {
     $logoutController = new LogoutController();
     $wishlistController = new WishlistController();
     $candidatureController = new CandidatureController();
-    $dashboardController = new DashboardController(); // Initialisation du contrôleur Dashboard
+    $dashboardController = new DashboardController(); 
 
     if (strpos($uri, 'cesi-lebonplan/') === 0) {
         $uri = substr($uri, strlen('cesi-lebonplan/'));
@@ -62,6 +61,11 @@ function route($uri) {
         case 'offres/cities':
             // Récupérer la liste des villes disponibles
             $offresController->cities();
+            break;
+            
+        case 'offres/featured':
+            // Récupérer les offres à la une
+            $offresController->featured();
             break;
 
         case 'dashboard': // Nouvelle route pour le dashboard étudiant
