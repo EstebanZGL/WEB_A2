dd682ee07a0a5409eaefa5d18d404e9965e2c839
EstebanZGL <esteban.ziegler@viacesi.fr>
2025-04-04 10:49:17 +0200

    test lecture cv

--- app/views/gestion/candidatures_etudiant.php
+++ app/views/gestion/candidatures_etudiant.php
@@ -208,7 +208,7 @@
                                                                 CV spécifique
                                                             </a>
                                                         <?php elseif (isset($etudiant['cv_path']) && !empty($etudiant['cv_path'])): ?>
-                                                            <a href="/<?php echo $etudiant['cv_path']; ?>" class="cv-link" target="_blank">
+                                                            <a href="/<?php echo "/var/www/cesi-lebonplan".$etudiant['cv_path']; ?>" class="cv-link" target="_blank">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                                                     <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
                                                                     <polyline points="14 2 14 8 20 8"></polyline>
